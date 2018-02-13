cdd40a37d60e188edd3db7e9690557b13048238d
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2018-02-12 22:54:14 -0500

    add funtion edit but have problem with redirect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,24 @@ class ProductsController < ApplicationController
 		redirect_to 'products#index'
 	end
 
+	def update
+		@product = Product.find(params[:id])
+		categories = Category.all
+
+		categories.each do |category|
+			if @product.categories.exists?("#{category.id}")
+				@product.categories.delete("#{category.id}")
+			end
+		end
+
+		params[:category_ids].each do |id|
+			@product.categories << Category.find(id)
+		end
+
+		@product.update(product_params)
+		redirect_to @product
+	end
+
 	private
 		def product_params
 			params.require(:product).permit(:name, :price, category_ids: [])
